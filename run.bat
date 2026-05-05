bd3f6ab1caa3e5a1cd588918aa5c0559e9495b2d
kirilltaganov63-prog <kirilltaganov63@gmail.com>
2026-05-05 23:04:45 +0300

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,4 @@
+@echo off
+set PYTHONPATH=%USERPROFILE%\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.13_qbz5n2kfra8p0\LocalCache\local-packages\Python313\site-packages
+%USERPROFILE%\AppData\Local\Microsoft\WindowsApps\PythonSoftwareFoundation.Python.3.13_qbz5n2kfra8p0\python.exe app.py
+pause
